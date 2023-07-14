5dc811c2a825b4477fae4a330c29f97b5fca5820
Rakibul Hasib <rakimhasib786@gmail.com>
2023-07-14 23:44:08 +0600

    SavoyIceCream Post Added

---
+++ StockManagement/DTO/SavoyIceCreamDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace StockManagement.DTO
+{
+    public class SavoyIceCreamDTO
+    {
+        public int SavoyIceCreamId { get; set; }
+        public string? ProductName { get; set; }
+        public int? Eja { get; set; }
+        public decimal Price { get; set; }
+        public int? NewProduct { get; set; }
+        public int? Total { get; set; }
+        public int? SalesQuantity { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public int? Dumping { get; set; }
+        public int? Receive { get; set; }
+        public int? Remaining { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
