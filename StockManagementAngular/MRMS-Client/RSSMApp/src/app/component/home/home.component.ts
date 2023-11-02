8561b3d9c6c2d4de9e5eea0bda4b3c0ff1c13e2d
Rakibul Hasib <rakimhasib786@gmail.com>
2023-11-02 10:15:27 +0600

    modified

--- StockManagementAngular/MRMS-Client/RSSMApp/src/app/component/home/home.component.ts
+++ StockManagementAngular/MRMS-Client/RSSMApp/src/app/component/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NotificationService } from '../../services/Shared/notification.service';
+import { DashboardServiceService } from 'src/app/services/dashboard/dashboard-service.service';
+import { Dashboarddata } from 'src/app/models/dashboard/dashboarddata';
 
 
 @Component({
@@ -9,17 +11,19 @@ import { NotificationService } from '../../services/Shared/notification.service'
 })
 export class HomeComponent {
 
+  dashboardModel: Dashboarddata=new Dashboarddata;
   constructor(
-    private notificationSvc: NotificationService
+    private notificationSvc: NotificationService,
+    private dashboardService: DashboardServiceService
   ) { }
 
   ngOnInit(): void {
-    //this.dashboardSvc.get()
-    //  .subscribe(x => {
-    //    this.dashboard = x;
-    //    console.log(x);
-    //  }, err => {
-    //    this.notificationSvc.message("Failed to load data!!!", "DISMISS");
-    //  });
+    this.dashboardService.getDashboardData()
+     .subscribe(x => {
+       this.dashboardModel = x;
+       console.log(x);
+     }, err => {
+       this.notificationSvc.message("Failed to load data!!!", "DISMISS");
+     });
   }
 }
---
+++ StockManagementAngular/MRMS-Client/RSSMApp/src/app/models/dashboard/dashboarddata.ts
@@ -0,0 +1,14 @@
+export class Dashboarddata {
+    constructor(
+        public savoySalesQ?: number,
+        public savoySalesP?: number,
+        public zanZeeSalesQ?: number,
+        public zanZeeSalesP?: number,
+        public lovelloSalesQ?: number,
+        public lovelloSalesP?: number,
+        public kaziFarmSalesQ?: number,
+        public kaziFarmSalesP?: number,
+        public iglooSalesQ?: number,
+        public iglooSalesP?: number,
+    ){}
+}
